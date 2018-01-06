6d249fe28db926f87fb7fae7fba1bc46424af731
scriptPilot <mail@scriptpilot.de>
2018-01-06 10:59:49 +0000

    default title

--- config.example.php
+++ config.example.php
@@ -3,6 +3,6 @@
   define('GOOGLE_API_KEY', '');
   define('GOOGLE_CLIENT_ID', '');
   define('GOOGLE_CLIENT_SECRET', '');
-  define('HTML_TITLE', '');
+  define('HTML_TITLE', 'Backup to Google Drive');
 
 ?>
