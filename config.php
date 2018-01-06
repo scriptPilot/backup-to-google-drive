dc171a4e74fa45bebd40a359fb5dc605d3fa9492
scriptPilot <mail@scriptpilot.de>
2018-01-06 02:57:33 +0000

    typ

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define('GOOGLE_API_KEY', 'AIzaSyBjSe2OWLtRav_WrW63Jw8TeFx-Ourg70w');
+  define('GOOGLE_CLIENT_ID', '422018760852-hdbkm2ikcojcn88ced8eautguofc5vkv.apps.googleusercontent.com');
+  define('GOOGLE_CLIENT_SECRET', 'rpO0-zv-dsGLrBjCG8Oznd7m');
+  define('HTML_TITLE', 'Backup to Google Drive');
+
+?>
